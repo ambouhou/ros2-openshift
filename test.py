796f427cc441d58484e40d4f97e056c12b8b6da4
ambouhou <abdel-malek.bouhou@student.unamur.be>
2022-08-05 18:14:42 +0200

    lest try again

--- test.py
+++ test.py
@@ -1,15 +1,1 @@
-#!/usr/bin/env python
-
-import rospy
-from std_msgs.msg import String
-
-def test():
-    pub = rospy.Publisher('test', String, queue_size=1)
-    rospy.init_node('test', anonymous=True)
-    pub.publish("ready")
-
-if __name__ == '__main__':
-    try:
-        test()
-    except rospy.ROSInterruptException:
-        pass
+print("Hello, World!")
